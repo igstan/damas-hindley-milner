c5f143f6dce05c90adf856d848afcca6b2031f99
Ionuț G. Stan <ionut.g.stan@gmail.com>
2015-03-29 16:29:58 +0300

    Implement constraint generator

--- src/main/scala/Annotate.scala
+++ src/main/scala/Annotate.scala
@@ -38,7 +38,40 @@ object Annotate {
 case class Constraint(a: Type, b: Type)
 
 object Constrain {
+  import Tysyn._
+
   def constrain(tysyn: Tysyn): List[Constraint] = {
-    ???
+    def loop(tysyns: List[Tysyn], constraints: List[Constraint]): List[Constraint] = {
+      tysyns match {
+        case Nil => constraints
+        case tysyn :: rest =>
+          tysyn match {
+            case INT(_, _) => loop(rest, constraints)
+            case ADD(_, a, b) => loop(a :: b :: rest, constraints)
+            case SUB(_, a, b) => loop(a :: b :: rest, constraints)
+            case BOOL(_, _) => loop(rest, constraints)
+            case VAR(_, _) => loop(rest, constraints)
+            case FN(_, _, body) => loop(body :: rest, constraints)
+            case IF(ty, test, yes, no) =>
+              val ifConstr = Constraint(ty, yes.ty)
+              val testConstr = Constraint(test.ty, Type.BOOL)
+              val branchConstr = Constraint(yes.ty, no.ty)
+              val newConstraints = ifConstr :: testConstr :: branchConstr :: constraints
+              loop(test :: yes :: no :: rest, newConstraints)
+            case APP(ty, fn, arg) =>
+              // Commented version is from lingua-002, make sure they both work the same.
+              // val appConstr = Constraint(fn.ty, Type.FN(arg.ty, ty))
+              val appConstr = Constraint(ty, Type.FN(arg.ty, fn.ty))
+              val newConstraints = appConstr :: constraints
+              loop(fn :: arg :: rest, newConstraints)
+            case LET(ty, binding, value, body) =>
+              val letConstr = Constraint(ty, body.ty)
+              val newConstraints = letConstr :: constraints
+              loop(value :: body :: rest, newConstraints)
+          }
+      }
+    }
+
+    loop(List(tysyn), List.empty[Constraint])
   }
 }
