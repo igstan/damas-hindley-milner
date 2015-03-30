a5dd03ffaf8f44969f2c538f55ff5c5831074d43
Ionuț G. Stan <ionut.g.stan@gmail.com>
2015-03-31 01:09:31 +0300

    Shorten match patterns in `annotate`

--- src/main/scala/Annotate.scala
+++ src/main/scala/Annotate.scala
@@ -3,29 +3,31 @@ package bucharestfp
 case class UnboundIdentifier(name: String) extends RuntimeException(s"unbound identifier: $name")
 
 object Annotate {
+  import Absyn._
+
   def annotate(absyn: Absyn, tenv: TypeEnv): Tysyn = {
     absyn match {
-      case Absyn.INT(value) => Tysyn.INT(Type.TINT, value)
-      case Absyn.ADD(a, b) => Tysyn.ADD(Type.TINT, annotate(a, tenv), annotate(b, tenv))
-      case Absyn.SUB(a, b) => Tysyn.SUB(Type.TINT, annotate(a, tenv), annotate(b, tenv))
-      case Absyn.BOOL(value) => Tysyn.BOOL(Type.TBOOL, value)
-      case Absyn.IF(test, yes, no) =>
+      case INT(value) => Tysyn.INT(Type.TINT, value)
+      case ADD(a, b) => Tysyn.ADD(Type.TINT, annotate(a, tenv), annotate(b, tenv))
+      case SUB(a, b) => Tysyn.SUB(Type.TINT, annotate(a, tenv), annotate(b, tenv))
+      case BOOL(value) => Tysyn.BOOL(Type.TBOOL, value)
+      case IF(test, yes, no) =>
         Tysyn.IF(Type.freshVar(), annotate(test, tenv), annotate(yes, tenv), annotate(no, tenv))
-      case Absyn.APP(fn, arg) =>
+      case APP(fn, arg) =>
         Tysyn.APP(Type.freshVar(), annotate(fn, tenv), annotate(arg, tenv))
-      case Absyn.FN(param, body) =>
+      case FN(param, body) =>
         val paramType = Type.freshVar()
         val extendedTenv = tenv.set(param, paramType)
         val annotatedBody = annotate(body, extendedTenv)
         val bodyType = annotatedBody.ty
         val fnType = Type.TFN(paramType, bodyType)
         Tysyn.FN(fnType, param, annotatedBody)
-      case Absyn.VAR(name) =>
+      case VAR(name) =>
         tenv.lookup(name) match {
           case None => throw UnboundIdentifier(name)
           case Some(ty) => Tysyn.VAR(ty, name)
         }
-      case Absyn.LET(binding, value, body) =>
+      case LET(binding, value, body) =>
         val annotatedValue = annotate(value, tenv)
         val extendedTenv = tenv.set(binding, annotatedValue.ty)
         val annotatedBody = annotate(body, extendedTenv)
