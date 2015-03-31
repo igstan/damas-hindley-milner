4efeb7cb1bceb33626c4e5b2656a64ffdb3dd8eb
Ionuț G. Stan <ionut.g.stan@gmail.com>
2015-03-31 05:44:28 +0300

    Rewrite Substitution.apply using an explicit recursive function

--- src/main/scala/Substitution.scala
+++ src/main/scala/Substitution.scala
@@ -4,9 +4,18 @@ case class Substitution(records: List[(Type.Var, Type)]) {
   import Type._
 
   def apply(ty: Type): Type = {
-    records.foldLeft(ty) { case (ty, (tvar, replacement)) =>
-      ty.substitute(tvar, replacement)
+    // records.foldLeft(ty) { case (ty, (tvar, replacement)) =>
+    //   ty.substitute(tvar, replacement)
+    // }
+    def loop(ty: Type, records: List[(Type.Var, Type)]): Type = {
+      records match {
+        case Nil => ty
+        case (tvar, replacement) :: rest =>
+          loop(ty.substitute(tvar, replacement), rest)
+      }
     }
+
+    loop(ty, records)
   }
 
   def compose(other: Substitution): Substitution = {
